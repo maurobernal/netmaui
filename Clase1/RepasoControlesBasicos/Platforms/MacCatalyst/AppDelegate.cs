04fb03746465a9f1ef35c1e410b6a7557b465613
mauro_bernal_rios <mauro_bernal_rios@hotmail.com>
2022-03-13 20:53:31 -0300

    corrección clase 1

--- Clase1/RepasoControlesBasicos/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace RepasoControlesBasicos;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
